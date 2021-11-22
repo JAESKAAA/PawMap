157dccaf6bfcff3d91f62dff53be5c5b89f5f970
JAESKAAA <jaeskaaa@gmail.com>
2021-11-22 11:39:41 +0900

    pawmap 기능 구현 관련 1차 초안

---
+++ PawMap/src/main/java/com/pawmap/PawMapApplication.java
@@ -0,0 +1,13 @@
+package com.pawmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PawMapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PawMapApplication.class, args);
+	}
+
+}
---
+++ PawMap/src/main/java/com/pawmap/VO/UserVO.java
@@ -0,0 +1,26 @@
+package com.pawmap.VO;
+
+import java.sql.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserVO {
+	private String user_id;
+	private String user_password;
+	private String user_email;
+	private String user_name;
+	private String user_tel_num;
+	private String address;
+	private String user_nickname;
+	private String type;
+	private Date user_reg_date;
+	private String user_profile;
+	private int user_profile_size;
+	private String user_status;
+	
+}
