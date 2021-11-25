a818a5e80c6aba8eddd3e5c7a22a607eaaa4ceef
JAESKAAA <jaeskaaa@gmail.com>
2021-11-25 14:42:35 +0900

    2차 병합-비밀번호찾기 및 헤더 푸터 따로 나눔

--- PawMap/src/main/java/com/pawmap/controller/BoardController.java
+++ PawMap/src/main/java/com/pawmap/controller/BoardController.java
@@ -1,15 +1,9 @@
 package com.pawmap.controller;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,9 +12,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.pawmap.VO.BoardVO;
-
 import com.pawmap.VO.CommentVO;
-
 import com.pawmap.VO.Criteria;
 import com.pawmap.VO.PageVO;
 import com.pawmap.service.BoardService;
