965dd5fc5914f8777359ee68730a7f9e0eacedb3
yh.thomas.lee <yh.thomas.lee@gmail.com>
2021-11-29 18:28:17 +0900

    updated the gradle project

---
+++ PawMap/src/test/java/com/pawmap/PawMapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pawmap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PawMapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
