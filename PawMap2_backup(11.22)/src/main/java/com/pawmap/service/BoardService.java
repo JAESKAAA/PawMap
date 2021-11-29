4683ab914451cd60e24bd69083e6be3921aaa77c
yh.thomas.lee <yh.thomas.lee@gmail.com>
2021-11-29 15:37:23 +0900

    first commit

--- PawMap2_backup(11.22)/src/main/java/com/pawmap/service/BoardService.java
+++
@@ -1,8 +0,0 @@
-package com.pawmap.service;
-
-import com.pawmap.VO.UserVO;
-
-public interface BoardService {
-
-	void insertTest(UserVO vo);
-}
--- PawMap2_backup(11.24)/src/main/java/com/pawmap/service/BoardService.java
+++
@@ -1,8 +0,0 @@
-package com.pawmap.service;
-
-import com.pawmap.VO.BoardVO;
-
-public interface BoardService {
-
-	void insertBoard(BoardVO vo);
-}
